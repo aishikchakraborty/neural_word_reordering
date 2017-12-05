2d0258079b9cb823b93287bc50dbe6c86253ca5c
Aishik Chakraborty <aishik@Aishiks-MacBook-Air.local>
2017-12-05 20:16:30 +0530

    Transformer codes added

--- src/hyperparams.py
+++ src/hyperparams.py
@@ -1,20 +1,26 @@
 class Hyperparams:
-    '''Set Hyperparameters'''
+    '''Hyperparameters'''
     # data
-    data = 'data/prose_clean.bpe.txt'
-
+    source_train = '../data/prose/src_train.txt'
+    target_train = '../data/prose/trg_train.txt'
+    source_test = '../data/prose/src_test.txt'
+    target_test = '../data/prose/trg_test.txt'
+    # source_train = '../data/preprocessed_data/prose.bpe.txt'
+    # target_train = '../data/preprocessed_data/prose.bpe.txt'
+    # source_test = '../data/preprocessed_data/prose.bpe.txt'
+    # target_test = '../data/preprocessed_data/prose.bpe.txt'
     # training
     batch_size = 32 # alias = N
     lr = 0.0001 # learning rate. In paper, learning rate is adjusted to the global step.
-    logdir = 'nwo_60' # log directory
+    logdir = '../nwo_40' # log directory
 
     # model
-    maxlen = 60 # Maximum number of words in a sentence. alias = T.
+    maxlen = 40 # Maximum number of words in a sentence. alias = T.
                 # Feel free to increase this if you are ambitious.
-    min_cnt = 0 # words whose occurred less than min_cnt are encoded as <UNK>.
-    hidden_units = 256 # alias = C
+    min_cnt = 10 # words whose occurred less than min_cnt are encoded as <UNK>.
+    hidden_units = 512 # alias = C
     num_blocks = 6 # number of encoder/decoder blocks
-    num_epochs = 1
+    num_epochs = 20
     num_heads = 8
     dropout_rate = 0.1
-    smoothing_rate = 0.
+    sinusoid = False # If True, use sinusoid. If false, positional embedding.
